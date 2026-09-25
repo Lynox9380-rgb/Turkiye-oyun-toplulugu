4b15ff6250ffb1456a261f15eeba948f7d367134
Lynox9380-rgb <salarnasrzade@gmail.com>
2026-09-26 01:07:18 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "turkiye-oyun-toplulugu",
+  "version": "1.0.0",
+  "description": "Türkiye Oyun Topluluğu Discord Botu",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.22.1"
+  }
+}
